e414dc413ec2cf5dbfd362d6aa661f74467591f7
KishoHiga <ivanbrening@gmail.com>
2026-07-29 21:14:43 +0300

    Loops and Conditionals

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main () {
+    int i = 1;
+    for (; i<10; i*=2) {
+        std::cout<<i<<'\n';
+        if (i<10) {
+            std::cout<<"Меньше 10"<<"\n";
+        }
+    }
+
+}
