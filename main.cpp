605c87546e8fd7dedb3b5caae156eb590075a386
KishoHiga <ivanbrening@gmail.com>
2026-07-30 16:21:05 +0300

    Multiple Lines in Strings

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 int main () {
-    int i = 1;
+    /*int i = 1;
+    if (i == 1) {
+        std::cout<<"Равно 1"<<"\n";
+    }
+    i += 1;
+    if (i != 2) {
+        std::cout<<"Не равно 2"<<"\n";
+    }
     for (; i<10; i*=2) {
         std::cout<<i<<'\n';
         if (i<10) {
             std::cout<<"Меньше 10!"<<"\n";
         }
-    }
+    }*/
+    int aleshka{200};
+    std::cout<<aleshka<<"\n"<<"Da nu"<<"\n"
+    <<"klinika"<<std::endl;
 
 }
